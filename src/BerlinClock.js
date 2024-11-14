12dd30fe3d53c47d3ead7daffa92e4692b34af13
elyazidigit <idris.elyazidi@student.vinci.be>
2024-11-14 15:03:35 +0100

    refactor getSingleMinutes

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -1,12 +1,8 @@
 export class BerlinClock {
 
-    getSingleMinutes(minutes) {
-    const lamps = ['0', '0', '0', '0'];
-    if (minutes % 5 >= 1) lamps[0] = 'Y';
-    if (minutes % 5 >= 2) lamps[1] = 'Y';
-    if (minutes % 5 >= 3) lamps[2] = 'Y';
-    if (minutes % 5 >= 4) lamps[3] = 'Y';
-    return lamps.join('');
-    
+     getSingleMinutes(minutes) {
+        const lampsOn = minutes % 5;
+        return 'Y'.repeat(lampsOn).padEnd(4, '0');
     }
+    
 }    
