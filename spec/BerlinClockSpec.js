95e8b931f8caecf63b239ddc25c465c34fa4635c
elyazidigit <idris.elyazidi@student.vinci.be>
2024-11-14 14:56:37 +0100

    test case YYYY

--- spec/BerlinClockSpec.js
+++ spec/BerlinClockSpec.js
@@ -7,7 +7,8 @@ describe("Berlin Clock Tests", function() {
     it("should display the correct single minute", function() {
         expect(clock.getSingleMinutes(0)).toBe('0000');
         expect(clock.getSingleMinutes(1)).toBe('Y000');
-        expect(clock.getSingleMinutes(4)).toBe('YYY0');
+        expect(clock.getSingleMinutes(4)).toBe('YYYY');
+        expect(clock.getSingleMinutes(5)).toBe('0000');
         
         
     });
--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -5,6 +5,7 @@ export class BerlinClock {
     if (minutes % 5 >= 1) lamps[0] = 'Y';
     if (minutes % 5 >= 2) lamps[1] = 'Y';
     if (minutes % 5 >= 3) lamps[2] = 'Y';
+    if (minutes % 5 >= 4) lamps[3] = 'Y';
     return lamps.join('');
     
     }
