30245caff01ed04f5dc8243653be706fc9816d8a
elyazidigit <idris.elyazidi@student.vinci.be>
2024-11-14 14:50:10 +0100

    test case Y000

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -3,6 +3,7 @@ export class BerlinClock {
     getSingleMinutes(minutes) {
     const lamps = ['0', '0', '0', '0'];
     if (minutes % 5 >= 1) lamps[0] = 'Y';
+    if (minutes % 5 >= 2) lamps[1] = 'Y';
     return lamps.join('');
     
     }
